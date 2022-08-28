d33b2724507f27b286882fb334b62c3130125f52
datsovtsvetan <datsovtsvetan@gmail.com>
2022-08-28 12:01:03 +0300

    sixth commit

--- src/Model/BaseClient.php
+++ src/Model/BaseClient.php
@@ -31,7 +31,7 @@ abstract class BaseClient
             $amount = $converter->convertToDefaultCurrency($amount, $currency);
         }
 
-        $key = $this->getYearAndWeekNumberKey($dateTime);
+        $key = $this->getWeekKey($dateTime);
 
         if (isset($this->withdrawsPerWeek[$key][self::AMOUNT])
             && isset($this->withdrawsPerWeek[$key][self::COUNT])) {
@@ -47,7 +47,7 @@ abstract class BaseClient
 
     public function getWithdrawnAmountByWeek(\DateTimeImmutable $date):float|int
     {
-        $key = $this->getYearAndWeekNumberKey($date);
+        $key = $this->getWeekKey($date);
         if(isset($this->withdrawsPerWeek[$key][self::AMOUNT])){
             return $this->withdrawsPerWeek[$key][self::AMOUNT];
         }
@@ -57,7 +57,7 @@ abstract class BaseClient
 
     public function getWithdrawnCountByWeek(\DateTimeImmutable $date):int
     {
-        $key = $this->getYearAndWeekNumberKey($date);
+        $key = $this->getWeekKey($date);
         if(isset($this->withdrawsPerWeek[$key][self::COUNT])) {
             return $this->withdrawsPerWeek[$key][self::COUNT];
         }
@@ -80,8 +80,58 @@ abstract class BaseClient
         return $this->withdrawsPerWeek;
     }
 
-    private function getYearAndWeekNumberKey(\DateTimeImmutable $dateTime):string
+    private function getWeekKey(\DateTimeImmutable $date):string
     {
-        return "year:".$dateTime->format('Y')."week:".$dateTime->format('W');
+
+        // check if is in the last week of year:
+        $year = $date->format('Y');
+        $lastDayOfYear = \DateTimeImmutable::createFromFormat("Y-m-d",
+            "$year-12-31");
+        $lastDayOfYearWeekNumber = $lastDayOfYear->format('W');
+        $weekNumber = $date->format('W');
+
+        $isOnLastWeek = ($weekNumber == $lastDayOfYearWeekNumber);
+
+        if($isOnLastWeek){
+            for ($i=0;$i<7;$i++){
+                $nextDate = $date->add(new \DateInterval('P'.$i.'D'))
+                    ->format('W');
+
+                if($nextDate != $weekNumber){
+                    $nextYear = (int) $year + 1;
+                    //var_dump($year."-".$nextYear);
+                    return $year."-".$nextYear;
+                }
+            }
+
+        }
+
+        //End check date is in last week of year
+
+        // Check for first week in year
+        /**
+         * to make sure the format is valid (i.e $date->format('W') => 1 or 01)
+         * and not brakes if format changes between php versions.
+        */
+        $firstWeekOfYear = \DateTimeImmutable::createFromFormat("Y-m-d",
+            "$year-01-01")->format('W');
+
+        $isOnFirstWeek = ($weekNumber == $firstWeekOfYear);
+
+        if($isOnFirstWeek){
+            for ($i=0;$i<7;$i++){
+                $prevDate = $date->sub(new \DateInterval('P'.$i.'D'))
+                    ->format('W');
+
+                if($prevDate != $weekNumber){
+                    $prevYear = (int) $year - 1;
+                    //var_dump($prevYear."-".$year);
+                    return $prevYear."-".$year;
+                }
+            }
+        }
+
+        //var_dump("year:".$date->format('Y')."week:".$date->format('W'));
+        return "year:".$date->format('Y')."week:".$date->format('W');
     }
 }
